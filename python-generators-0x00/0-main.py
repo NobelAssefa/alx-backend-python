b7a904c20626ba9b4430c023e0a57d75511b5ff7
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-19 16:00:48 +0300

    Update 0-main.py

--- python-generators-0x00/0-main.py
+++ python-generators-0x00/0-main.py
@@ -1,1 +1,13 @@
 
+seed = __import__('seed')
+
+connection = seed.connect_db()
+if connection:
+    seed.create_database(connection)  # ✅ creates ALX_prodev
+    connection.close()
+
+    connection = seed.connect_to_prodev()  # ✅ connects to ALX_prodev
+
+    if connection:
+        seed.create_table(connection)     # ✅ creates user_data table
+        seed.insert_data(connection, 'user_data.csv')  # ✅ inserts CSV data
