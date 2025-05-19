00879e8b47fef86f75256a3e9ea0f3bd71cf69f1
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-19 16:18:14 +0300

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,12 @@
+seed = __import__('seed')
 
+
+def stream_users():
+    connection1 = seed.connect_db()
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    for row in cursor:
+        yield row
+    cursor.close()
+    connection.close()
